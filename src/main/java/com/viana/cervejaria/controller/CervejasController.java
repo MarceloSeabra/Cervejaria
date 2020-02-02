3209d7890b90a75923f0fff825823bbe585e2b9d
Marcelo Viana <marcelo.vms@outlook.com>
2020-02-02 20:21:16 -0300

    Configurando o Thymeleaf

--- src/main/java/com/viana/cervejaria/controller/CervejasController.java
+++ src/main/java/com/viana/cervejaria/controller/CervejasController.java
@@ -3,8 +3,16 @@
  */
 package com.viana.cervejaria.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.viana.cervejaria.model.Cerveja;
 
 /**
  * @author marcelo
@@ -16,6 +24,22 @@ public class CervejasController {
 	@RequestMapping("/cervejas/novo")
 	public String novo() {
 		return "cerveja/CadastroCerveja";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+		if(bindingResult.hasErrors()) {
+			//System.out.println("Tem erro sim " + bindingResult.getFieldError().getDefaultMessage());
+			
+			model.addAttribute("mensagem",bindingResult.getFieldError().getDefaultMessage());
+			return "cerveja/CadastroCerveja";
+		}
+		
+		redirectAttributes.addFlashAttribute("mensagem","Cerveja Cadastrada com Sucesso");
+		System.out.println("SKU " + cerveja.getSku());
+		System.out.println("NOME " + cerveja.getNome());
+		System.out.println("DESCRIÇÃO " + cerveja.getDescricao());
+		return "redirect:/cervejas/novo";
 	}
 
 }
---
+++ src/main/java/com/viana/cervejaria/model/Cerveja.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.viana.cervejaria.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * @author marcelo
+ *
+ */
+public class Cerveja {
+
+	@NotBlank(message = "Sku é obrigatório")
+	private String sku;
+	@NotBlank(message = "O nome é obrigatório")
+	private String nome;
+	@NotBlank(message = "Descrição é obrigatória")
+	@Size(max = 50, min = 10)
+	private String descricao;
+	
+	public Cerveja() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return the sku
+	 */
+	public String getSku() {
+		return sku;
+	}
+
+	/**
+	 * @param sku the sku to set
+	 */
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the descricao
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * @param descricao the descricao to set
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
